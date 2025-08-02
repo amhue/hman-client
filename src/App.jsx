c378b0b1bc2e23137ee3e553c8b05d11f4173c47
Aritra Mondal <aritramondal855@gmail.com>
2025-08-02 14:50:06 +0530

    deploy 2

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,7 @@ export default function App() {
 
     if (user != null && user.mgmt) {
         return (
-            <div className="min-h-screen flex flex-col max-w-screen">
+            <div className="min-h-screen flex flex-col">
                 <BrowserRouter>
                     <main className="flex-1">
                         <Routes>
@@ -68,7 +68,7 @@ export default function App() {
     }
 
     return (
-        <div className="min-h-screen flex flex-col max-w-screen">
+        <div className="min-h-screen flex flex-col">
             <BrowserRouter>
                 <Navbar user={user} />
                 <main className="flex-1">
